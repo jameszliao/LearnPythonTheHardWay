a3b19d7dc8ceaa08a5bcc2246d92bb4a738415da
James Liao <james.liao@outlook.com>
2022-03-07 13:22:00 -0800

    ex51 input to flaskweb form

--- projects/flaskweb/app.py
+++ projects/flaskweb/app.py
@@ -1,17 +1,44 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+# @app.route('/')
+@app.route('/hello', methods=['POST', 'GET'])
 
 # def hello_world():
 #     greeting = "Azure SDK"
 #     return f'Hello, {greeting}!'
 
+# def index():
+#     greeting = "Azure SDK"
+#     return render_template("index.html", greeting=greeting)
+
 def index():
-    greeting = "Azure SDK"
-    return render_template("index.html", greeting=greeting)
+
+    # name = request.args.get('name', 'Nobody')
+    # http://127.0.0.1:5000/hello?name=
+
+    # greet = request.args.get('greet', 'Hello')
+    # http://127.0.0.1:5000/hello?name=&greet=
+
+    # if name:
+    #     greeting = f"{greet}, {name}"
+    # else:
+    #     greeting = "Hellow Azure SDK"
+
+    # return render_template("index.html", greeting=greeting)
+
+    greeting = "Hello World"
+
+    if request.method == "POST":
+        name = request.form['name']
+        greet = request.form['greet']
+        greeting = f"{greet}, {name}"
+        return render_template("index.html", greeting=greeting)
+    else:
+        return render_template("hello_form.html")
 
 if __name__ == "__main__":
     app.run()

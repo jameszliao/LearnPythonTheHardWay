41c56b493e5eb2c5adc62fe1c668756fb32d6597
James Liao <james.liao@outlook.com>
2022-03-07 12:27:07 -0800

    flaskweb with index.html

--- projects/flaskweb/app.py
+++ projects/flaskweb/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 
-def hello_world():
+# def hello_world():
+#     greeting = "Azure SDK"
+#     return f'Hello, {greeting}!'
+
+def index():
     greeting = "Azure SDK"
-    return f'Hello, {greeting}!'
+    return render_template("index.html", greeting=greeting)
 
 if __name__ == "__main__":
     app.run()

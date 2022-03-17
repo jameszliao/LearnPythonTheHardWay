f23f4b0928c64687584d2fd6d7742e0dde3daee1
James Liao <james.liao@outlook.com>
2022-03-17 14:04:53 -0700

    Farewell message

--- ex09.py
+++ ex09.py
@@ -1,14 +1,43 @@
 # Here's some new strange stuff, remember type it exactly.
 
-days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# days = "Mon Tue Wed Thu Fri Sat Sun"
+# months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
-print("Here are the days:", days)
-print("Here are the months: ", months)
+# print("Here are the days:", days)
+# print("Here are the months: ", months)
 
-print("""
-There's something going on here.
-With the three double-quotes.
-We'll be able to type as much as we like.
-Even 4 lines if we want, or 5, or 6.
+# print("""
+# There's something going on here.
+# With the three double-quotes.
+# We'll be able to type as much as we like.
+# Even 4 lines if we want, or 5, or 6.
+# """)
+
+years_in_microsoft = 10
+
+new_opportunity = "a tech startup focusing on doing good to do well, maximizing both economic and social impacts"
+
+linkedin = "https://www.linkedin.com/in/jameszliao/"
+
+personal_email = "JAMES.LIAO@OUTLOOK.COM"
+
+print(f"""
+Dear Comrades,
+
+After more than {years_in_microsoft} years working at Microsoft,
+and the only company I worked for in my life,
+it is with mixed feelings that I announce that my last day at Microsoft is tomorrow.
+
+Starting on Monday, I will be pursuing a once-in-a-lifetime opportunity to join {new_opportunity}.
+
+I wanted to thank each of you for the positive impact on my professional growth.
+
+I am hoping that our paths cross once again.
+
+{linkedin}
+
+{personal_email}
+
+Thank you and warm regards,
+James Liao
 """)
